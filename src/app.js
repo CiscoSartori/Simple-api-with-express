b8074290f0e2b6e831ef321fe2f1001097919736
CiscoSartori <lulucaantono@gmail.com>
2023-01-24 22:46:33 -0300

    adjusting getters, post, put end delete

--- src/app.js
+++ src/app.js
@@ -2,20 +2,46 @@ import express from "express";
 
 const app = express();
 
-const livros = [
-  {id: 1, "titulo": "Senhor dos Aneis"},
-  {id: 2, "titulo": "O Hobbit"}
+app.use(express.json())
+
+const Bikes = [
+  {id: 1, "titulo": "Senhor esteja comvosco"},
+  {id: 2, "titulo": "Ele esta no meio de nois"}
 ]
+
+
+app.get('/busca/:id', (req, res) => {
+  let index = buscaBike(req.params.id);
+  res.json(Bikes[index]);
+})
 
 app.get('/', (req, res) => {
   res.status(200).send('Cursinho Pika');
 }) 
 
-app.get('/Busca', (req, res) => {
-  res.status(200).send('Quero Cafe')
+app.get('/busca', (req, res) => {
+  res.status(200).send('Quero Cafe'+ res.json(Bikes))
 })
 
+app.post('/busca', (req, res) =>{
+  Bikes.push(req.body);
+  res.status(201).send('o duty e bilhonario')
+})
 
+app.put('/busca/:id', (req, res) => {
+  let index = buscaBike(req.params.id);
+  Bikes[index].titulo = req.body.titulo;
+  res.json(Bikes[index]);
+})
 
+app.delete('/busca/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaBike(id);
+  Bikes.splice(index, 1);
+  res.send('bike foi de base');
+})
+function buscaBike(id) {
+  return Bikes.findIndex(Bikes => Bikes.id == id)
+}
 
 export default app
